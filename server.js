7dbf22ae5e95b15b5f564fcb96a060b94c301616
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-21 11:01:16 +0530

    CHapter - 2. Read and write files

--- server.js
+++ server.js
@@ -1,21 +1,73 @@
-const os = require("os");
+const fsPromises = require("fs").promises;
 const path = require("path");
-const { add, subtract, divided, multiply } = require("./math");
 
-console.log(add(5, 10));
-console.log(subtract(10, 5));
-console.log(divided(10, 5));
-console.log(multiply(10, 5));
+console.log("Hello...💥");
 
-// console.log(os.type())
-// console.log(os.version())
-// console.log(os.homedir())
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "car.txt"),
+      "utf-8"
+    );
+    console.log(data);
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "newCar.txt"),
+      data
+    );
+    await fsPromises.appendFile(
+      path.join(__dirname, "files", "newCar.txt"),
+      "\n\nFord Mustang GT-500"
+    );
+    await fsPromises.rename(
+      path.join(__dirname, "files", "newCar.txt"),
+      path.join(__dirname, "files", "RenameNewCar.txt")
+    );
 
-// console.log(__dirname)
-// console.log(__filename)
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "files", "promiseComplete.txt"),
+      "utf-8"
+    );
+    console.log(newData);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
+fileOps();
 
-// console.log(path.parse(__filename))
+// fs.readFile(path.join(__dirname, "files", "lorem.txt"), "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.writeFile(
+//   path.join(__dirname, "files", "reply.txt"),
+//   "Nice to meet you😊",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File created");
+//   }
+// );
+
+// fs.readFile(
+//   path.join(__dirname, "files", "reply.txt"),
+//   "utf-8",
+//   (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   }
+// );
+
+// fs.appendFile(
+//   path.join(__dirname, "files", "car.txt"),
+//   "lamborghini",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Append file");
+//   }
+// );
+
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncaught error: ${err}`);
+  process.exit(1);
+});
