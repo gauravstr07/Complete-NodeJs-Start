5d43fe8ca99c4a6c5a763a5fe83a8266d4702d78
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-21 18:45:35 +0530

    Complete Middleware 😍

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const path = require("path");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Custom middleware logger
+app.use(logger);
+const whiteList = ["https://www.google.com/"];
+const corsOption = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOption));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname });
@@ -43,15 +64,23 @@ const two = (req, res, next) => {
 const three = (req, res, next) => {
   res.send("Finished");
   console.log("Calling three");
-
   next();
 };
 
 app.get("/chain(.html)?", [one, two, three]);
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not found");
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server running on port: ${port}📡`);
